f962ff2e778d0c7b91754f7a4d9863a1b9b0b055
regorxxx <regorxxx@protonmail.com>
2025-12-05 16:31:15 +0100

    - Helpers: updated helpers.

--- buttons/buttons_playback_order.js
+++ buttons/buttons_playback_order.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-//04/12/25
+//05/12/25
 
 /*
 	Playback controls
@@ -11,8 +11,6 @@ include('..\\helpers\\buttons_xxx.js');
 /* global getUniquePrefix:readable, buttonsBar:readable, addButton:readable, ThemedButton:readable */
 include('..\\helpers\\buttons_xxx_menu.js');
 /* global settingsMenu:readable  */
-include('..\\helpers\\helpers_xxx_flags.js');
-/* global PlaybackOrder:readable */
 include('..\\helpers\\helpers_xxx_UI.js');
 /* global chars:readable */
 include('..\\helpers\\helpers_xxx_properties.js');
@@ -47,16 +45,25 @@ addButton({
 		prefix, buttonsProperties: newButtonsProperties,
 		icon: function () {
 			switch (plman.PlaybackOrder) {
-				case PlaybackOrder.Default: return '\uf064';
-				case PlaybackOrder.RepeatPlaylist: return '\uf079' + 'ᵖˡˢᵗ';
-				case PlaybackOrder.RepeatTrack: return '\uf079' + 'ᵗʳᵃᶜᵏ';
-				case PlaybackOrder.Random: return chars.shuffle;
-				case PlaybackOrder.ShuffleTracks: return chars.shuffle + 'ᵗʳᵃᶜᵏˢ';
-				case PlaybackOrder.ShuffleAlbums: return chars.shuffle + 'ᵃˡᵇᵘᵐˢ';
-				case PlaybackOrder.ShuffleFolders: return chars.shuffle + 'ᶠᵒˡᵈᵉʳˢ';
+				case this.playbackOrder.Default: return '\uf064';
+				case this.playbackOrder.RepeatPlaylist: return '\uf079' + 'ᵖˡˢᵗ';
+				case this.playbackOrder.RepeatTrack: return '\uf079' + 'ᵗʳᵃᶜᵏ';
+				case this.playbackOrder.Random: return chars.shuffle;
+				case this.playbackOrder.ShuffleTracks: return chars.shuffle + 'ᵗʳᵃᶜᵏ';
+				case this.playbackOrder.ShuffleAlbums: return chars.shuffle + 'ᵃˡᵇᵘᵐ';
+				case this.playbackOrder.ShuffleFolders: return chars.shuffle + 'ᶠᵒˡᵈᵉʳ';
 			}
 		},
 		variables: {
+			playbackOrder: {
+				Default: 0,
+				RepeatPlaylist: 1,
+				RepeatTrack: 2,
+				Random: 3,
+				ShuffleTracks: 4,
+				ShuffleAlbums: 5,
+				ShuffleFolders: 6
+			},
 			eventListeners: [],
 			setCallbacks: function (parent, add) {
 				if (add) {
@@ -67,17 +74,17 @@ addButton({
 			},
 			getName: function(parent, val = plman.PlaybackOrder) {
 				switch (val) {
-					case PlaybackOrder.Default: return 'Default';
-					case PlaybackOrder.RepeatPlaylist: return'Repeat playlist';
-					case PlaybackOrder.RepeatTrack: return'Repeat track';
-					case PlaybackOrder.Random: return'Random';
-					case PlaybackOrder.ShuffleTracks: return'Shuffle tracks';
-					case PlaybackOrder.ShuffleAlbums: return'Shuffle albums';
-					case PlaybackOrder.ShuffleFolders: return'Shuffle folders';
+					case this.playbackOrder.Default: return 'Default';
+					case this.playbackOrder.RepeatPlaylist: return'Repeat playlist';
+					case this.playbackOrder.RepeatTrack: return'Repeat track';
+					case this.playbackOrder.Random: return'Random';
+					case this.playbackOrder.ShuffleTracks: return'Shuffle tracks';
+					case this.playbackOrder.ShuffleAlbums: return'Shuffle albums';
+					case this.playbackOrder.ShuffleFolders: return'Shuffle folders';
 				}
 			},
 			cycle: function(parent, i = 1, from = plman.PlaybackOrder) {
-				const values = Object.values(PlaybackOrder);
+				const values = Object.values(this.playbackOrder);
 				values.rotate(values.indexOf(from) + i);
 				return plman.PlaybackOrder = values[0];
 			}
