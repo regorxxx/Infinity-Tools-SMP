64f8ac6d1983fe7152f4a30555faa74e8c6f80b9
regorxxx <regorxxx@protonmail.com>
2025-12-23 17:56:50 +0100

    - Importing file: added codepage popup when imported file is detected as a non UTF-8 file by Windows/SMP, so the user can force an specific codepage or rely on the detected one. See [here](https://hydrogenaudio.org/index.php/topic,128838.msg1074594.html#msg1074594).

--- main/playlist_tools/playlist_tools_menu_other_tools.js
+++ main/playlist_tools/playlist_tools_menu_other_tools.js
@@ -55,7 +55,7 @@
 								}
 								let codePage = utils.DetectCharset(path);
 								if (codePage !== utf8) {
-									codePage = convertCharsetToCodepage(Input.string('string', 'UTF-8', 'Automatic codepage detection has identified a non UTF-8 file.\n\nYou can force a codepage below or use the one detected by clicking cancel (' + codePage + '):\n', window.FullPanelName, 'UTF-8', void (0), true) || Input.lastInput ) || codePage || utf8;
+									codePage = convertCharsetToCodepage(Input.string('string', 'UTF-8', 'Automatic codepage detection has identified a non UTF-8 file.\n\nYou can force a codepage below or use the one detected by clicking cancel (' + codePage + '):\n', window.FullPanelName, 'UTF-8', void (0), true) || (Input.isLastEqual ? Input.lastInput : '')) || codePage || utf8;
 								}
 								let formatMask = Input.string(
 									'string',
